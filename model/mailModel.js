cb8c61415b0cbff3f38e76e213edb5517aee2022
Amit <shekharjvm@gmail.com>
2021-01-29 17:48:01 +0530

    removed comments

--- model/mailModel.js
+++ model/mailModel.js
@@ -29,52 +29,7 @@ const mailSchema = new mongoose.Schema({
         default: Date.now()
     }
     
-}/*,{
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true }
-}*/);
-
-// userSchema.pre(/find/, function(next){
-//     this.find({ active: {$ne: false }})
-//     next();
-// });
-
-// userSchema.pre('save', async function(next){
-//     if(!this.isModified('password')) return next();
-
-//     this.password = await bcrypt.hash(this.password, 12);
-//     this.confirmPassword = undefined;
-//     next();
-// });
-
-// userSchema.pre('save', function(next) {
-//     if(!this.isModified('password') || this.isNew) return next();
-
-//     this.passwordChangedAt = Date.now() - 1000;
-//     next();
-// })
-
-// userSchema.methods.correctPassword = async function (candidatePassword, userPassword){
-//     return  await bcrypt.compare(candidatePassword, userPassword);
-// };
-
-// userSchema.methods.chnagedPasswordAfter = function(JWTTimestamp){
-//     if(this.passwordChangedAt){
-//         const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
-//         return JWTTimestamp < changedTimestamp;
-//     };
-//     //FALSE means password not changed
-//     return false;
-// };
-
-// userSchema.methods.createPasswordResetToken = function(){
-//     const resetToken = crypto.randomBytes(32).toString('hex');
-
-//     this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
-    
-//     this.passwordResetExpires = Date.now() + 10*60*1000;
-//     return resetToken;
-// };
+});
 
 const Mail = mongoose.model('Mail', mailSchema);
 
